df7bd7ec12baf81eda612c6c4ba05322651abb25
ragugcp <ragu.gcplabs@gmail.com>
2026-02-23 20:51:15 -0500

    added python learnings

---
+++ week1/python_basics/week1_datatypes.py
@@ -0,0 +1,25 @@
+#########################Data types in Python#########################
+# String
+name = "Ragu"
+print("The name is " + name)
+# Integer
+age = 39
+print("The age is " + str(age))
+# Float
+height = 5.9
+print("The height is " + str(height))
+# Boolean
+is_student = True
+print("Is the person a student? " + str(is_student))
+# List
+fruits = ["apple", "banana", "cherry"]
+print("The fruits are " + str(fruits))
+# Tuple
+coordinates = (10, 20)
+print("The coordinates are " + str(coordinates))
+# Dictionary
+person = {"name": "Ragu", "age": 39, "height": 5.9}
+print("The person details are " + str(person))
+# Set
+unique_numbers = {1, 2, 3, 4, 5}
+print("The unique numbers are " + str(unique_numbers))
